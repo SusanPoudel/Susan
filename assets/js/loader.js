fbd130ce25fdf6a583e25f4dfc0ebcb454367d61
SusanPoudel <111071491+SusanPoudel@users.noreply.github.com>
2024-04-16 17:52:07 +0545

    js adjusment

---
+++ assets/js/loader.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+    const preloader = document.querySelector('#preloader');
+    if (preloader) {
+      window.addEventListener('load', () => {
+        setTimeout(() => {
+          preloader.classList.add('loaded');
+        }, 1000);
+        setTimeout(() => {
+          preloader.remove();
+        }, 2000);
+      });
+    }
+    function aos_init() {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+      });
+    }
+    window.addEventListener('load', () => {
+      aos_init();
+    });
+  
+  });
